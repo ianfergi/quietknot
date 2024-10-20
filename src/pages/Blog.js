757ca43da1e7911c991fd0fa4144251b00f63b00
Ian Ferguson <ianferguson@ians-mbp.lan>
2024-10-20 17:49:49 -0400

    update blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import SideNav from "../components/sideNav";
-import { detectMob, routeTo } from "../utils";
+import { detectMob } from "../utils";
 import ScreenContainer, { VARIANT } from "../components/screenContainer";
 import yaml from 'js-yaml'; 
 import Masonry from 'masonry-layout';
@@ -27,12 +27,25 @@ const Screen = styled.div`
   }
 `;
 
+// Function to format content with a caret on lines with text, and skip empty lines
+const formatContentForRendering = (content) => {
+  const lines = content.split('\n');
+  return lines.map((line) => {
+    // Only add ">" to lines that have content; skip empty lines
+    if (line.trim() !== '') {
+      return `> ${line.trim()}`; // Add caret and trim unnecessary spaces
+    } else {
+      return ''; // Return empty string for empty lines (no caret)
+    }
+  }).join('\n'); // Join the lines back together with newlines
+};
+
 const Blog = () => {
     const isMobile = detectMob();
 
     const [blogs, setBlogs] = useState([]);
 
-    const onClose = (key) => {
+    const onClose = () => (key) => {
       // Filter out the blog with the given key
       const updatedBlogs = blogs.filter(blog => blog.key !== key);
       setBlogs(updatedBlogs); // Update the state
@@ -74,11 +87,11 @@ const Blog = () => {
             <Screen>
               <Container className={'grid-container'} isMobile={isMobile} >
                 {blogs.map((blog, index) => (
-                  <Card1 key={index} onClose={() => onClose(blog?.key)} className={'card'} variant={VARIANT.V2} title={blog?.title} size={blog?.size} isDraggable={false}>
+                  <Card1 key={index} onClose={onClose(blog?.key)} className={'card'} variant={VARIANT.V2} title={blog?.title} size={blog?.size} isDraggable={false}>
                     {blog?.date}
                     <br />
                     <br />
-                    {blog?.content}
+                    {formatContentForRendering(blog?.content)}
                     </Card1>
                 ))}
               </Container>
